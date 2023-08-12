000f0d3a892ae54f0c917eeb0e27e2c87c4ac842
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-08-12 09:31:50 +0530

    new component is added for Contribution page

---
+++ src/pages/ContributionComponent/ProfileCard.tsx
@@ -0,0 +1,32 @@
+// ProfileCard.jsx
+
+import React from 'react';
+import './ProfileCard.css';
+import { IonIcon } from '@ionic/react';
+import { logoTwitter, logoLinkedin } from 'ionicons/icons';
+
+const ProfileCard = ({ profileData }) => {
+    const { image, message, name, socialLinks } = profileData;
+
+    return (
+        <div className="profile-card">
+            <div className="profile-image">
+                <img src={image} alt="Profile" />
+            </div>
+            <div className="profile-details">
+                <h2>{name}</h2>
+                <p>{message}</p>
+                <div className="social-links">
+                    <a href={socialLinks.twitter} target="_blank" rel="noopener noreferrer">
+                        <IonIcon icon={logoTwitter} />
+                    </a>
+                    <a href={socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
+                        <IonIcon icon={logoLinkedin} />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileCard;
---
+++ src/pages/ContributionComponent/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import ProfileCard from './ProfileCard';
+import Layout from '@theme/Layout';
+import { IonReactRouter } from '@ionic/react-router';
+
+const ContributionComponent = () => {
+    const profileDataList = [
+        {
+            image: 'https://avatars.githubusercontent.com/u/99037494?v=4',
+            message: `Hello, I'm a web developer!`,
+            name: 'Ajay Dhangar',
+            socialLinks: {
+                twitter: 'https://twitter.com/AJAYDHA27250016',
+                linkedin: 'https://www.linkedin.com/in/ajay-dhangar/',
+            },
+        },
+        // Add more profiles as needed
+        {
+            image: 'https://avatars.githubusercontent.com/u/101887417?v=4',
+            message: `Hello, I'm another developer!`,
+            name: 'Shivaya',
+            socialLinks: {
+                twitter: '/',
+                linkedin: '/',
+            },
+        },
+        {
+            image: 'https://avatars.githubusercontent.com/u/101887417?v=4',
+            message: `Hello, I'm another developer!`,
+            name: 'Shivaya',
+            socialLinks: {
+                twitter: '/',
+                linkedin: '/',
+            },
+        },
+        {
+            image: 'https://avatars.githubusercontent.com/u/101887417?v=4',
+            message: `Hello, I'm another developer!`,
+            name: 'Shivaya',
+            socialLinks: {
+                twitter: '/',
+                linkedin: '/',
+            },
+        },
+        {
+            image: 'https://avatars.githubusercontent.com/u/101887417?v=4',
+            message: `Hello, I'm another developer!`,
+            name: 'Shivaya',
+            socialLinks: {
+                twitter: '/',
+                linkedin: '/',
+            },
+        },
+        // Add more profiles...
+    ];
+
+    return (
+        <Layout wrapperClassName='main-code-container'>
+            <IonReactRouter>
+                <div className="Main-Profile-Page">
+                    {profileDataList.map((profileData, index) => (
+                        <ProfileCard key={index} profileData={profileData} />
+                    ))}
+                </div>
+            </IonReactRouter>
+        </Layout>
+    );
+};
+
+export default ContributionComponent;
