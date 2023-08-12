ddacd48638bddbcb2b67aea932381a926750f33f
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-08-12 09:58:33 +0530

    new component is added for Contribution page

--- src/pages/ContributionComponent/ProfileCard.tsx
+++ src/pages/ContributionComponent/ProfileCard.tsx
@@ -8,21 +8,21 @@ const ProfileCard = ({ profileData }) => {
         return null;
     }
 
-    const { image, message, name, socialLinks } = profileData;
+    // const { image, message, name, socialLinks } = profileData;
 
     return (
         <div className="profile-card">
             <div className="profile-image">
-                <img src={image} alt="Profile" />
+                <img src={profileData.image} alt="Profile" />
             </div>
             <div className="profile-details">
-                <h2>{name}</h2>
-                <p>{message}</p>
+                <h2>{profileData.name}</h2>
+                <p>{profileData.message}</p>
                 <div className="social-links">
-                    <a href={socialLinks.twitter} target="_blank" rel="noopener noreferrer">
+                    <a href={profileData.socialLinks.twitter} target="_blank" rel="noopener noreferrer">
                         <IonIcon icon={logoTwitter} />
                     </a>
-                    <a href={socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
+                    <a href={profileData.socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
                         <IonIcon icon={logoLinkedin} />
                     </a>
                 </div>
