062b119899ffef5e729993215ed4de919318ae62
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-08-14 15:56:45 +0530

    update

--- src/pages/DonationComponent/index.tsx
+++ src/pages/DonationComponent/index.tsx
@@ -7,16 +7,16 @@ import Link from '@docusaurus/Link';
 
 const PaymentComponent = () => {
     const paymentOptions = [
-        { amount: 5, link: '/' },
-        { amount: 10, link: 'https://paytm.business/link/1660380018610/LL_538994547' },
+        { amount: 5, link: 'https://paytm.business/link/1692008456082/LL_644845305' },
+        { amount: 10, link: 'https://paytm.business/link/1692008498913/LL_644845335' },
         { amount: 25, link: 'https://paytm.business/link/1691718042886/LL_644057655' },
-        { amount: 50, link: '/' },
+        { amount: 50, link: 'https://paytm.business/link/1691890883744/LL_644585140' },
         { amount: 100, link: 'https://paytm.business/link/1691717913115/LL_644057646' },
-        { amount: 200, link: '/' },
-        { amount: 500, link: '/' },
-        { amount: 1000, link: '/' },
-        { amount: 5000, link: '/' },
-        { amount: 10000, link: '/' },
+        { amount: 200, link: 'https://paytm.business/link/1691891049654/LL_644585150' },
+        { amount: 500, link: 'https://paytm.business/link/1691890942459/LL_644585144' },
+        { amount: 1000, link: 'https://paytm.business/link/1691891737811/LL_644585183' },
+        { amount: 5000, link: 'https://paytm.business/link/1691930112498/LL_644665515' },
+        { amount: 10000, link: 'https://paytm.business/link/1692008590258/LL_644845375' },
         // Add more payment options here
     ];
 
