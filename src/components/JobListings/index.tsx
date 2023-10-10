d39d4ef9b2ad362f3ccbed0a6a4c249f6dcb57d9
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-10-10 17:48:32 +0530

    Update index.tsx (➕ competitions)
    competitions
    
    Signed-off-by: Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>

--- src/components/JobListings/index.tsx
+++ src/components/JobListings/index.tsx
@@ -6,6 +6,13 @@ const JobListings = () => {
   // Example job data organized in an array of objects
   const jobs = [
     {
+      jobTitle: 'Virtual Stock Simulation Competition',
+      company: 'Delhi Technological University (DTU), New Delhi',
+      jobDescription: `Explore the exciting world of stock trading in our Stock Simulation Trading Competition! This three-day event, taking place from October 11th to 13th, offers a unique opportunity for individuals to test their trading skills in an inter-college competition setting. Participants have a chance to win awards worth 100,000 and an incredible internship opportunity with StockGro. It's the perfect chance for aspiring traders to gain real market experience without risking their capital.`,
+      applyLink: 'https://unstop.com/competitions/virtual-stock-simulation-competition-dtu-new-delhi-792664?lb=wKFhVkE&utm_medium=Share&utm_source=shortUrl',
+      isVerified: true, // This company is verified
+    },
+    {
       jobTitle: 'Software Engineer, Front End, Google Cloud',
       company: 'Google',
       jobDescription: '1 year of experience with DSA, and software development in one or more programming languages (e.g., Python, C, C++, Java, Javascript).',
@@ -19,20 +26,20 @@ const JobListings = () => {
       applyLink: 'https://forms.gle/LsEQKm7UJCUnaqPA6',
       isVerified: true, // This company is verified
     },
-    {
-      jobTitle: 'Backend Developer',
-      company: 'CMHQ',
-      jobDescription: 'Join our backend development team...',
-      applyLink: 'https://codemastermindhq.vercel.app/',
-      isVerified: true, // This company is verified
-    },
-    {
-      jobTitle: 'Video Editor',
-      company: 'CMHQ',
-      jobDescription: 'Join our backend development team...',
-      applyLink: 'https://codemastermindhq.vercel.app/',
-      isVerified: false, // This company is note verified
-    },
+    // {
+    //   jobTitle: 'Backend Developer',
+    //   company: 'CMHQ',
+    //   jobDescription: 'Join our backend development team...',
+    //   applyLink: 'https://codemastermindhq.vercel.app/',
+    //   isVerified: true, // This company is verified
+    // },
+    // {
+    //   jobTitle: 'Video Editor',
+    //   company: 'CMHQ',
+    //   jobDescription: 'Join our backend development team...',
+    //   applyLink: 'https://codemastermindhq.vercel.app/',
+    //   isVerified: false, // This company is note verified
+    // },
     
     // Add more job listings here
   ];
