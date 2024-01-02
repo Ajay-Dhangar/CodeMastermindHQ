964b2be0a4a70c05e681d99d4d19aef65971019d
Ajay-Dhangar <ajaydhangar49@gmail.com>
2024-01-02 16:25:14 +0530

    update course cards

--- src/components/Courses/ReactCourses/index.tsx
+++ src/components/Courses/ReactCourses/index.tsx
@@ -1,61 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import clsx from 'clsx';
-import { Card, CardBody, CardTitle, CardText, CardImg, Button } from 'reactstrap';
-import styles from './styles.module.css';
-
+import './styles.css';
 
 const ReactCourses = () => {
     return (
-        <div className={clsx('container', styles.course_container)}>
-            <div className={clsx('row', styles.course_row)}>
-                <div className={clsx("col-md-3 col-sm-6", styles.course_card)}>
-                    <Card>
-                        <CardImg top width="100%" src="https://i.udemycdn.com/course/240x135/1362070_b9a1_2.jpg" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle className={clsx(styles.course_card_title)}>Complete Intro to React</CardTitle>
-                            <CardText className={clsx(styles.course_card_text)}>Learn React basics like JSX, Components, State, Props, and Hooks by building projects!</CardText>
+        <div className="course_container">
+            <div className="course_row">
+                <div className="course_card">
+                    <div>
+                        <img width="100%" src="https://i.udemycdn.com/course/240x135/1362070_b9a1_2.jpg" alt="Card image cap" />
+                        <div>
+                            <p className="course_card_title">Complete Intro to React</p>
+                            <p className="course_card_text">Learn React basics like JSX, Components, State, Props, and Hooks by building projects!</p>
                             <Link to="/courses/category/complete-intro-to-react">
-                                <Button color="primary">Start Course</Button>
+                                <button color="primary" className='course_card_button'>Start Course</button>
                             </Link>
-                        </CardBody>
-                    </Card>
+                        </div>
+                    </div>
                 </div>
-                <div className={clsx("col-md-3 col-sm-6", styles.course_card)}>
-                    <Card>
-                        <CardImg top width="100%" src="https://i.udemycdn.com/course/240x135/705264_caa9_7.jpg" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle className={clsx(styles.course_card_title)}>React Hooks</CardTitle>
-                            <CardText className={clsx(styles.course_card_text)}>Learn the new way of building React apps: hooks, which cover useState, useEffect, and useRef.</CardText>
+                <div className="course_card">
+                    <div>
+                        <img width="100%" src="https://i.udemycdn.com/course/240x135/705264_caa9_7.jpg" alt="Card image cap" />
+                        <div>
+                            <p className="course_card_title">React Hooks</p>
+                            <p className="course_card_text">Learn the new way of building React apps: hooks, which cover useState, useEffect, and useRef.</p>
                             <Link to="/courses/">
-                                <Button color="primary">Start Course</Button>
+                                <button color="primary" className='course_card_button'>Start Course</button>
                             </Link>
-                        </CardBody>
-                    </Card>
+                        </div>
+                    </div>
                 </div>
-                <div className={clsx("col-md-3 col-sm-6", styles.course_card)}>
-                    <Card>
-                        <CardImg top width="100%" src="https://i.udemycdn.com/course/240x135/705264_caa9_13.jpg" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle className={clsx(styles.course_card_title)}>React Native</CardTitle>
-                            <CardText className={clsx(styles.course_card_text)}>Learn how to build mobile apps with react native using react-navigation, firebase, and redux!</CardText>
+                <div className="course_card">
+                    <div>
+                        <img width="100%" src="https://i.udemycdn.com/course/240x135/705264_caa9_13.jpg" alt="Card image cap" />
+                        <div>
+                            <p className="course_card_title"> React Advance </p>
+                            <p className="course_card_text">
+                                Learn to build scalable React applications using the latest tools and techniques available in the React ecosystem!
+                            </p>
                             <Link to="/courses/">
-                                <Button color="primary">Start Course</Button>
+                                <button color="primary" className='course_card_button'>Start Course</button>
                             </Link>
-                        </CardBody>
-                    </Card>
+                        </div>
+                    </div>
                 </div>
-                <div className={clsx("col-md-3 col-sm-6", styles.course_card)}>
-                    <Card>
-                        <CardImg top width="100%" src="https://i.udemycdn.com/course/240x135/705264_caa9_2.jpg" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle className={clsx(styles.course_card_title)}>React Native</CardTitle>
-                            <CardText className={clsx(styles.course_card_text)}>Learn how to build mobile apps with react native using react-navigation, firebase, and redux!</CardText>
+                <div className="course_card">
+                    <div>
+                        <img width="100%" src="https://i.udemycdn.com/course/240x135/705264_caa9_2.jpg" alt="Card image cap" />
+                        <div>
+                            <p className="course_card_title">React Mastery</p>
+                            <p className="course_card_text">
+                                Learn React from scratch with Redux and Firebase(database) to build web apps with Authentication and CRUD features.
+                            </p>
                             <Link to="/courses/">
-                                <Button color="primary">Start Course</Button>
+                                <button color="primary" className='course_card_button'>Start Course</button>
                             </Link>
-                        </CardBody>
-                    </Card>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
